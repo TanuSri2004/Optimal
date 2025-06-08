4a558dbf166adaf7de9eb2168ebf4d11d49b92d3
Tanusri2004 <tanusripalawar@gmail.com>
2025-06-08 11:20:18 +0530

    Optimal Ambulance System

---
+++ Optimal_Ambulance_Positioning/optimal_ambulance_positioning/evn/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\91778\Downloads\B7_Optimal_Ambulance_Positioning\Optimal_Ambulance_Positioning\optimal_ambulance_positioning\evn\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
